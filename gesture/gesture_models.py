c2dd2195ef5287d957ab01ba4b0e91d848d2ae32
wenyang2 <limwenyangxd@gmail.com>
2025-04-10 03:59:59 +0800

    added L2 regularization

--- gesture/gesture_models.py
+++ gesture/gesture_models.py
@@ -5,15 +5,17 @@ import torch.nn.functional as F
 # Multi-layer Perceptron --> a class of feedforward ANN
 class mlp_classifier(nn.Module):
     #num_classes is how many labels I have
-    def __init__(self, input_size=63, hidden_size=128, num_classes=10):
+    def __init__(self, input_size=63, hidden_size=128, num_classes=10,dropout_rate=0.5):
         super(mlp_classifier, self).__init__()
         #nn.linear(in, out)
         self.fc1=nn.Linear(input_size, hidden_size)
         self.fc2=nn.Linear(hidden_size, hidden_size)
         self.fc3=nn.Linear(hidden_size, num_classes)
+        self.dropout = nn.Dropout(dropout_rate)
 
     def forward(self,x):
         x=F.relu(self.fc1(x))
+        x=self.dropout(x)
         x=F.relu(self.fc2(x))
         x=self.fc3(x)
 
